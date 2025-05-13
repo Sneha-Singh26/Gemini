563593acfd99882ea4dcc39c97362b1f26ddf6fe
Sneha-Singh26 <ssnehasingh12340@gmail.com>
2025-05-13 16:00:11 +0530

    change file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Sidebar from './components/sidebar/sidebar'
 import Main from './components/main/main'
-
-const App = () => {
-  return (
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+const App= () =>{
+  return(
     <>
-      <Sidebar  />
-      <Main />
+     <Sidebar/>
+     <Main />
     </>
   )
 }
